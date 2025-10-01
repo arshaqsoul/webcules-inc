c683a5aec92a382da728f43d5cdf807b51f3fee7
Arshaq <arshaqsoul@gmail.com>
2025-10-01 13:10:18 -0600

    feat: use storage-s3 in prod

--- packages/payload/src/plugins/index.ts
+++ packages/payload/src/plugins/index.ts
@@ -3,6 +3,7 @@ import { nestedDocsPlugin } from "@payloadcms/plugin-nested-docs";
 import { redirectsPlugin } from "@payloadcms/plugin-redirects";
 import { seoPlugin } from "@payloadcms/plugin-seo";
 import { searchPlugin } from "@payloadcms/plugin-search";
+import { s3Storage } from "@payloadcms/storage-s3";
 import { Plugin } from "payload";
 import { revalidateRedirects } from "@webcules/payload/hooks/revalidateRedirects";
 import { GenerateTitle, GenerateURL } from "@payloadcms/plugin-seo/types";
@@ -95,6 +96,24 @@ export const plugins: Plugin[] = [
     searchOverrides: {
       fields: ({ defaultFields }) => {
         return [...defaultFields, ...searchFields];
+      },
+    },
+  }),
+  s3Storage({
+    enabled: !process.env.NEXT_PUBLIC_SERVER_URL?.includes("localhost"),
+    collections: {
+      media: {
+        disableLocalStorage: true,
+      },
+    },
+    bucket: process.env.R2_BUCKET || "",
+
+    config: {
+      region: process.env.R2_REGION,
+      endpoint: process.env.R2_ENDPOINT,
+      credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
       },
     },
   }),
